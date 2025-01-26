5bf3994e801135fa7eb148192fe584c236874520
felipe <felipe.baleche2@gmail.com>
2025-01-26 11:20:43 -0300

    improved code readability

--- src/ast.rs
+++ src/ast.rs
@@ -3,6 +3,11 @@ use define_macro::define;
 use crate::token::Token;
 
 define! {
+    enum stmt ->  exprStmt(ExprStmt)
+                | printStmt(PrintStmt);
+
+    struct exprStmt -> expr(Expr);
+    struct printStmt -> expr(Expr);
     enum expr ->  unary(Unary)
                 | binary(Binary)
                 | grouping(Box<Expr>)
@@ -11,12 +16,4 @@ define! {
     struct unary -> operator(Token), right(Box<Expr>);
     struct binary -> left(Box<Expr>), operator(Token), right(Box<Expr>);
     enum literal -> str(String) | number(f64) | bool(bool) | null;
-}
-
-define! {
-    enum stmt ->  exprStmt(ExprStmt)
-                | printStmt(PrintStmt);
-
-    struct exprStmt -> expr(Expr);
-    struct printStmt -> expr(Expr);
 }
